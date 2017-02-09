d5cfb2010660c9e7d1a355586dc75ee36f45d9a5
Casey Marshall <github@cmars.tech>
2017-02-08 22:20:19 -0600

    Add tests, doc comments, some cleanup.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate serde_derive;
+
 extern crate futures;
 extern crate notify;
 extern crate serde;
@@ -7,17 +10,24 @@ extern crate chrono;
 
 use std::sync::mpsc;
 use std::error::Error as StdError;
-use std::ops::Drop;
 use std::os::unix::fs::{DirBuilderExt, OpenOptionsExt};
 
 use serde::{Serialize, Deserialize};
 
+/// A durable queue backed by the filesystem.
+///
+/// This queue stores items of type T as files in a spool directory. Files are serialized with
+/// serde_json so T must derive serde's Serialize and Deserialize traits.
+///
+/// The queue's directory should only contain items of the same type. Any files in the spool
+/// that fail to deserialize will be discarded.
 pub struct Queue<T> {
     path: String,
     _placeholder: std::marker::PhantomData<T>,
 }
 
 impl<T: Serialize + Deserialize> Queue<T> {
+    /// Create a new `Queue<T>` using the given directory path for storage.
     pub fn new(path: &str) -> Result<Queue<T>, std::io::Error> {
         std::fs::DirBuilder::new().recursive(true).mode(0o700).create(path)?;
         Ok(Queue::<T> {
@@ -26,11 +36,10 @@ impl<T: Serialize + Deserialize> Queue<T> {
         })
     }
 
+    /// Push an item into the Queue.
     pub fn push(&self, item: T) -> Result<(), std::io::Error> {
         let mut item_path = std::path::PathBuf::from(&self.path);
-        let item_name = format!("{}-{}",
-                                chrono::UTC::now().timestamp(),
-                                textnonce::TextNonce::sized_urlsafe(32).unwrap().into_string());
+        let item_name = format!("{}-{}", chrono::UTC::now().timestamp(), rand_string());
         item_path.push(item_name);
         let complete_path = item_path.to_str().unwrap();
         item_path.with_extension("inc");
@@ -50,6 +59,12 @@ impl<T: Serialize + Deserialize> Queue<T> {
         Ok(())
     }
 
+    /// Pop an item off the queue without regard to ordering.
+    ///
+    /// This method returns the first matching directory entry. Queue ordering cannot be
+    /// guaranteed, as the serialized file is chosen based on the filesystem's ordering.
+    ///
+    /// Use this method when you value speed over order-correctness.
     pub fn pop(&self) -> Result<Option<T>, std::io::Error> {
         let dirh = std::fs::read_dir(&self.path)?;
         for maybe_dirent in dirh {
@@ -60,7 +75,7 @@ impl<T: Serialize + Deserialize> Queue<T> {
             {
                 let item_file = std::fs::OpenOptions::new().read(true)
                     .open(&item_path)?;
-                let _cleanup = Cleanup::File(item_path.to_str().unwrap().to_string());
+                let _cleanup = cleanup::Cleanup::File(item_path.to_str().unwrap().to_string());
                 let maybe_item: Result<T, serde_json::error::Error> =
                     serde_json::from_reader(item_file);
                 match maybe_item {
@@ -72,6 +87,16 @@ impl<T: Serialize + Deserialize> Queue<T> {
         Ok(None)
     }
 
+    /// Pull the next queued item off the queue.
+    ///
+    /// This method returns the next item, based on the system time when the item was pushed
+    /// onto the queue. Note that system time may not always be in monotonically increasing order,
+    /// so this is not strictly guaranteed.
+    ///
+    /// This method may be much slower, especially if the queue size grows large.
+    ///
+    /// If multiple threads or processes are pulling from the same queue directory, globally
+    /// duplicates may occur, since reading the item's file and removing it is not atomic.
     pub fn pull(&self) -> Result<Option<T>, std::io::Error> {
         let dirh = std::fs::read_dir(&self.path)?;
         let mut items: Vec<_> = dirh.filter(|item| match item {
@@ -94,7 +119,7 @@ impl<T: Serialize + Deserialize> Queue<T> {
         {
             let item_file = std::fs::OpenOptions::new().read(true)
                 .open(item_path)?;
-            let _cleanup = Cleanup::File(item_path.to_str().unwrap().to_string());
+            let _cleanup = cleanup::Cleanup::File(item_path.to_str().unwrap().to_string());
             let maybe_item: Result<T, serde_json::error::Error> =
                 serde_json::from_reader(item_file);
             match maybe_item {
@@ -105,6 +130,7 @@ impl<T: Serialize + Deserialize> Queue<T> {
     }
 }
 
+/// Process a Queue<T> as a stream of future values.
 pub struct QueueStream<T> {
     queue: Queue<T>,
     _watcher: notify::RecommendedWatcher,
@@ -112,6 +138,7 @@ pub struct QueueStream<T> {
 }
 
 impl<T: Serialize + Deserialize> QueueStream<T> {
+    /// Create a new QueueStream<T> with the given spool path.
     pub fn new(path: &str) -> Result<QueueStream<T>, std::io::Error> {
         let queue = Queue::<T>::new(path)?;
         let (tx, rx) = mpsc::channel();
@@ -132,6 +159,9 @@ impl<T: Serialize + Deserialize> futures::stream::Stream for QueueStream<T> {
     type Item = T;
     type Error = std::io::Error;
 
+    /// Attempt to pull the next item off the stream.
+    ///
+    /// This method polls the underlying filesystem watcher for changes since the last poll.
     fn poll(&mut self) -> futures::Poll<Option<Self::Item>, Self::Error> {
         match self.rx.try_recv() {
             Ok(_) => {
@@ -151,21 +181,99 @@ fn to_ioerror(e: &StdError) -> std::io::Error {
     std::io::Error::new(std::io::ErrorKind::Other, e.description())
 }
 
-enum Cleanup {
-    File(String),
-}
+mod cleanup {
+    use std;
+    use std::ops::Drop;
 
-impl Drop for Cleanup {
-    fn drop(&mut self) {
-        match self {
-            &mut Cleanup::File(ref path) => {
-                match std::fs::remove_file(path) {
-                    Ok(_) => {}
-                    Err(e) => {
-                        println!("warning: failed to remove file {}: {}", path, e);
+    pub enum Cleanup {
+        File(String),
+        #[allow(dead_code)]
+        Dir(String),
+    }
+
+    impl Drop for Cleanup {
+        fn drop(&mut self) {
+            match self {
+                &mut Cleanup::File(ref path) => {
+                    match std::fs::remove_file(path) {
+                        Ok(_) => {}
+                        Err(e) => {
+                            println!("warning: failed to remove file {}: {}", path, e);
+                        }
+                    }
+                }
+                &mut Cleanup::Dir(ref path) => {
+                    match std::fs::remove_dir_all(path) {
+                        Ok(_) => {}
+                        Err(e) => {
+                            println!("warning: failed to remove file {}: {}", path, e);
+                        }
                     }
                 }
             }
         }
+    }
+}
+
+fn rand_string() -> String {
+    textnonce::TextNonce::sized_urlsafe(32).unwrap().into_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use std;
+    use super::*;
+
+    #[derive(Serialize, Deserialize)]
+    #[derive(PartialEq,Debug)]
+    struct Foo {
+        i: i32,
+        b: bool,
+        s: String,
+    }
+
+    fn new_queue() -> (Queue<Foo>, cleanup::Cleanup) {
+        let mut spool_path_buf = std::env::temp_dir();
+        spool_path_buf.push(rand_string());
+        let spool_dir = spool_path_buf.to_str().unwrap();
+        let _cleanup = cleanup::Cleanup::Dir(spool_dir.to_string());
+        let q = Queue::<Foo>::new(spool_dir).unwrap();
+        (q, _cleanup)
+    }
+
+    #[test]
+    fn test_push_pop() {
+        let (q, _cleanup) = new_queue();
+        assert!(q.push(Foo {
+                i: 999,
+                b: true,
+                s: "foo".to_string(),
+            })
+            .is_ok());
+        let result = q.pop().unwrap().unwrap();
+        assert_eq!(result,
+                   Foo {
+                       i: 999,
+                       b: true,
+                       s: "foo".to_string(),
+                   });
+    }
+
+    #[test]
+    fn test_push_pull() {
+        let (q, _cleanup) = new_queue();
+        assert!(q.push(Foo {
+                i: 888,
+                b: false,
+                s: "bar".to_string(),
+            })
+            .is_ok());
+        let result = q.pop().unwrap().unwrap();
+        assert_eq!(result,
+                   Foo {
+                       i: 888,
+                       b: false,
+                       s: "bar".to_string(),
+                   });
     }
 }
